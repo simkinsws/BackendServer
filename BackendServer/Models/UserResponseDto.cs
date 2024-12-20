5294a056407f6fff72919f419824287cbe48e9c0
Nir Simkin <nir.simkin@gmail.com>
2024-12-20 16:54:07 +0200

    added UserREsponseDto for future using .

---
+++ BackendServer/Models/UserResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BackendServer.Models
+{
+    public class UserResponseDto
+    {
+        public string? Id { get; set; }
+        public string? UserName { get; set; }
+        public string? CreatedAt { get; set; }
+        public string? LastUpdatedAt { get; set; }
+        public string? Email { get; set; }
+        public int Posts { get; set; }
+
+    }
+}
